d9ab43d2a62bbe5766193470c8e6497f3b2f0241
Hari Ohm Prasth Rajagopal <hariohmprasath@gmail.com>
2023-05-13 02:02:53 -0700

    Updated commit with comments

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM --platform=linux/amd64 amazonlinux:latest 
 RUN yum install -y python3 python3-pip jq unzip
+# Install AWS CLI v2
 RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 RUN unzip awscliv2.zip
 RUN ./aws/install
+# Install Hardeneks
 RUN python3 -m venv /tmp/.venv
 RUN source /tmp/.venv/bin/activate
 RUN pip install hardeneks
+# Setup the scan script as entry point
 COPY run.sh /run.sh
 RUN chmod +x /run.sh
 CMD ["sh","run.sh"]
--- run.sh
+++ run.sh
@@ -1,11 +1,18 @@
+# Setup the environment variables
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 export BUCKET_NAME=hardeneks-report-output-$AWS_ACCOUNT_ID
+
+# List all the EKS clusters in the region
 aws eks list-clusters > clusters.json --region $AWS_REGION
+
+# Check for the bucket and create it if it doesn't exist
 if aws s3api head-bucket --bucket $BUCKET_NAME 2>/dev/null; then
     echo "Bucket exists"
 else
     aws s3api create-bucket --bucket $BUCKET_NAME --region $AWS_REGION --create-bucket-configuration LocationConstraint=$AWS_REGION
 fi
+
+# Loop through the clusters and run hardeneks on each one, generate the report and upload it to S3
 for cluster in $(jq -r '.clusters[]' clusters.json); do
     aws eks update-kubeconfig --region $AWS_REGION --name $cluster
     hardeneks --cluster $cluster --region $AWS_REGION --export-txt $cluster.txt
